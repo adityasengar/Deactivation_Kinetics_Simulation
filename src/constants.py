5760d4541f8a1d5153c6fa11501c53be51b22773
Aditya Sengar <adityasengariitd@gmail.com>
2021-03-12 11:00:00 +0100

    feat: Define species and placeholder rate constants

---
+++ src/constants.py
@@ -0,0 +1,10 @@
+# Species as defined in the SI, page 2, X vector
+SPECIES = [
+    "C+", "iC4+", "H+", "H+", "C+", "iC4", "C", "C3", "C7", "Cn="
+]
+
+# Default rate constants (placeholders)
+DEFAULT_RATES = {
+    "k1": 1.0, "k2": 1.0, "k3": 0.1, "k4": 1.0, "k5": 0.1,
+    "k6": 0.01, "k7": 0.01, "k9": 10.0, "k10": 1.0, "k11": 0.01
+}
