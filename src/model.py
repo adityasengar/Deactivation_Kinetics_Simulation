206f0562fd74911060742f317a5c25f2d2f6f6c2
Aditya Sengar <adityasengariitd@gmail.com>
2021-03-25 16:00:00 +0100

    refactor: Rename species for clarity

--- src/model.py
+++ src/model.py
@@ -2,15 +2,15 @@ import numpy as np
 
 def calculate_rate_vector(y, k):
     """Calculates the reaction rate vector 'v'."""
-    C3_plus, iC4_plus, H_plus, H_plus_2, C_plus, iC4, C, C3, C7, Cn_eq = y
+    C3_plus, iC4_plus, H_plus, H_plus_2, C_plus_species, iC4, C, C3, C7, Cn_eq = y
     
     # Rate expressions v from SI, page 2
     v = np.array([
         k['k1'] * C3,          # v1
         k['k2'] * iC4_plus,    # v2
         k['k3'] * iC4_plus,    # v3
-        k['k4'] * C_plus,      # v4
-        k['k5'] * C_plus,      # v5
+        k['k4'] * C_plus_species,      # v4
+        k['k5'] * C_plus_species,      # v5
         k['k6'] * H_plus * iC4, # v6
         k['k7'] * H_plus * C,   # v7
         0,                     # v8 is not in S matrix
